a50e498dc1e8e28fb096e5255f96126c26947c44
TreeGee73 <TreeGee73@gmail.com>
2020-11-23 08:24:50 -0700

    completed front end design, begin JS debug and development

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
-    $('#search-button').on('click', function() {
-        var searchValue = $('#search-value').val();
+// Variables
+    let date = new Date();
 
-        // 
+$(document).ready(function(){
+    $('#search-button').on('click', function() {
+        let city = $('#search-value').val();
+        // Clear the search box text
         $('#search-value').val('');
-        
-        searchWeather(searchValue);
+    searchWeather(city);
     });
 
     $('.history').on('click', 'li', function() {
@@ -13,22 +14,26 @@ $(document).ready(function() {
     });
 
     function makeRow(text) {
-        var li = $('<li>').addClass('list-group-item list-group-item-action').text(text);
+        let li = $('<li>').addClass('list-group-item list-group-item-action').text(text);
         $('.history').append(li);
     }
+});
 
-    function searchWeather(searchValue) {        const apikey = "554845f58eea39e31d7b61cfe4d53c53";         const query = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apikey}&units=imperial`;
+    function searchWeather(city) {
+    const apikey = "554845f58eea39e31d7b61cfe4d53c53";
+    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+        // const query = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
         $.ajax({
             type: 'GET',
-            url: query,
+            url: queryUrl,
             dataType: 'json',
             success: function(data) {
                 // 
-                if (history.indexOf(searchValue) === -1) {
-                    history.push(searchValue);
+                if (history.indexOf(city) === -1) {
+                    history.push(city);
                     window.localStorage.setItem('history', JSON.stringify(history));
 
-                    makeRow(searchValue);
+                    makeRow(city);
                 }
 
                 // Clear
@@ -41,7 +46,7 @@ $(document).ready(function() {
                 var humid = $('<p>').addClass('card-text').text('Humidity: ' + data.main.humidity + '%');
                 var temp = $('<p>').addClass('card-text').text('Temperature: ' + data.main.temp + ' &ordm; F');
                 var cardBody = $('<div>').addClass('card-body');
-                var img = $('<img>').attr('src', 'https://openweathermap.org/img/w/' + data.weather[0].);
+                var img = $('<img>').attr('src', 'https://openweathermap.org/img/wn//' + data.weather[0].icon + "@2x.png");
 
                 // Merge content and add to page
                 title.append(img);
@@ -50,20 +55,20 @@ $(document).ready(function() {
                 $('#today').append(card);
 
                 // Follow-up api endoint calls
-                getForcast(searchValue);
+                getForcast(city);
                 getUVIndex(data.cord.lat, data.coord.lon);
             }
         });
     }
 
-    function getForcast(searchValue) {        const apikey = "554845f58eea39e31d7b61cfe4d53c53";         const query = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apikey}&units=imperial`;
+    function getForcast(city) {        const apikey = "554845f58eea39e31d7b61cfe4d53c53";         const query = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
     $.ajax({
         type: 'GET',
         url: query,
         dataType: 'json',
         success: function(data) {
             // Replace existing content
-            $('#forecast').html('<h4 class=\'mt-3\'>5-Day Forecast:</h4>').append('<div class=\'row\'>');
+            $('#forecast').html('<h5 class=\'mt-3\'>5-Day Forecast:</h5>').append('<div class=\'row\'>');
 
             // 3-Hour forcast loop
             for (var i = 0; i < data.list.length; i++) {
@@ -74,16 +79,11 @@ $(document).ready(function() {
                     var card = $('<div>').addClass('card');
 
 
-                    var wind = $('<p>').addClass('card-text').text('Wind Speed: ' + data.wind.speed + 'mph');
-                    var humid = $('<p>').addClass('card-text').text('Humidity: ' + data.main.humidity + '%');
-                    var temp = $('<p>').addClass('card-text').text('Temperature: ' + data.main.temp + ' &ordm; F');
-                    var cardBody = $('<div>').addClass('card-body');
-                    var img = $('<img>').attr('src', 'https://openweathermap.org/img/w/' + data.weather[0].);
 
 
-
-
-
+                    var title = $('<h1>').addClass('card-text').text(data.list[i].date_text);
+                    var img = $('<img>').attr('src', 'https://openweathermap.org/img/w/' + data.weather[0].icon);
+                    var p1 = $('<p>').addClass('card-text').text('Temperature: ' + data.main.temp + ' &ordm; F');
                     var p2 = $('<p>').addClass('card-text').text('Humidity: ' + data.list[i].main.humidity + '%');
 
                     // Merge content and add to page
@@ -95,7 +95,27 @@ $(document).ready(function() {
         });
     }
 
-    function getUVIndex(lat, lon) {
+    function getUVIndex(lat, lon) {        const apikey = "554845f58eea39e31d7b61cfe4d53c53";         const query = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
+    $.ajax({
+        type: 'GET',
+        url: query,
+        dataType: 'json',
+        success: function(data) {
+            var uv = $('<p>').text('UV Index: ');
+            var btn = $('<span>').addClass('btn btn-sm').text(data.value);
 
+            // UV value set color
+            if (data.value < 3) {
+                btn.addClass('btn-success');
+            } 
+            else if (data.value < 7) {
+                btn.addClass('btn-warning');
+            } 
+            else {
+                btn.addClass('btn-danger');
+            }
+
+            $('#today.cardbody').append(uv.append(btn));
+        }
+        })
     }
-});
