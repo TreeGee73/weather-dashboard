b7bcce5111438a3b3bd3e3fd78accea80d73de8e
TreeGee73 <TreeGee73@gmail.com>
2020-11-28 16:15:10 -0700

    added 5 day script, formatted HTML and CSS

--- assets/script.js
+++ assets/script.js
@@ -31,6 +31,9 @@ $('#searchBtn').on('click', function() {
 
         // Run Current Conditions Function & Display in 'Today' Section on HTML
         getCurrentConditions(response);
+
+        // Run 5-Day Forecast Function & Display in '5-Day Forcast' Section on HTML
+        getCurrentForecast(response);
     })
 });
 
@@ -60,3 +63,46 @@ function getCurrentConditions(response) {
         $('<p>').addClass('card-text current-wind').text('Wind Speed: ' + response.wind.speed + ' MPH')
         )
   }
+
+// Function to Create and Display the 5-Day Forecast
+function getCurrentForecast() {
+    // Execute API Search & Return Data
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + apiKey,
+        method: 'GET'
+    }).then(function (response){
+        // Removes Existing Content & Child Nodes from the '5-Day Forecast' Div
+        $('#forecast').empty();
+        // Set Variable to Collect Response Data
+        let results = response.list;
+        // Create For Loop to Capture Forecast Data for 'Today'+1 through 'Today'+5
+      //declare start date to check against
+      // startDate = 20
+      //have end date, endDate = startDate + 5
+      for (let i = 0; i < results.length; i++) {
+        // Set Date/Time Variable  
+        let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+        let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+
+        // Get Data Based on Dates for Forecast
+        if(results[i].dt_txt.indexOf('00:00:00') !== -1){
+            // Get and Convert Temperature to Degrees Farenheit
+            let tempF = (results[i].main.temp - 273.15) * 1.80 + 32;
+            // Rounds Temperature to the Nearest Degree
+           tempF = Math.round(tempF);
+
+            // Get, Format, and Add Current Forcast Data to '5-Day Forecast' Div
+            $('#forecast').append(
+                $('<div>').addClass('card mr-2  ml-3 forecast-card').append(
+                    $('<div>').addClass('card-body').append(
+                        $('<h5>').addClass('card-title').text(date.toLocaleDateString('en-US')),
+                        $('<img>').addClass('forecast-img').attr('src', 'https://openweathermap.org/img/w/' + results[i].weather[0].icon + '.png'),
+                        $('<p>').addClass('card-text').text('Temperature: ' + tempF + ' °F'),
+                        $('<p>').addClass('card-text').text('Humidity: ' + results[i].main.humidity + '%')
+                    )
+                )
+            )
+        }
+    }
+});
+}
